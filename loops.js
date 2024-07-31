c99169cff4b7c7be7970f0998a2de96c5d1b248d
Yoojinjo <eugenejo111@gmail.com>
2024-07-31 15:27:11 -0500

    some code

---
+++ loops.js
@@ -0,0 +1,18 @@
+// Part 1
+// for (let i = 1; i < 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("Fizz Buzz");
+//     } else if (i % 3 == 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 == 0) {
+//         console.log("Buzz");
+//     }
+//     else (
+//         console.log(i)
+//     )
+// }
+
+//Part2
+let n=4
+i=2
+for (let i=2; n%i==0; i++)
